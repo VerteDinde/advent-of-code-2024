1ad8fa2e53da11dd7e34ad1376ed3671d4349f9c
VerteDinde <vertedinde@electronjs.org>
2024-12-07 22:28:06 -0800

    build: day 7 pt 2

--- typescript/src/07/index.ts
+++ typescript/src/07/index.ts
@@ -1,14 +1,26 @@
 import input from "./input";
 
-function isCorrectCalibration({ result, values }): boolean {
-  for (let i = 0; i < 2 ** values.length; i++) {
+function isCorrectCalibration(
+  // @ts-ignore
+  { result, values },
+  checkThirdOperator: boolean,
+): boolean {
+  const max = checkThirdOperator ? 3 ** values.length : 2 ** values.length;
+
+  for (let i = 0; i < max; i++) {
     let acc = values[0];
     let currIndex = 1;
-    const str = i.toString(2).padStart(values.length - 1, "0");
+    const str = checkThirdOperator
+      ? i.toString(3).padStart(values.length - 1, "0")
+      : i.toString(2).padStart(values.length - 1, "0");
 
     for (const e of str) {
       if (e === "0") acc *= values[currIndex];
-      else acc += values[currIndex];
+      else if (e === "1") acc += values[currIndex];
+      else {
+        const str = ["", acc, values[currIndex]].join("");
+        acc = Number(str);
+      }
       if (acc > result) break;
       currIndex++;
     }
@@ -21,9 +33,16 @@ function isCorrectCalibration({ result, values }): boolean {
 function main() {
   let result = 0;
   for (const calibration of input) {
-    if (isCorrectCalibration(calibration)) result += calibration.result;
+    if (isCorrectCalibration(calibration, false)) result += calibration.result;
   }
-  console.log("Result: ", result);
+  console.log("Result Pt 1: ", result); // Part 1 answer
+
+  let resultpt2 = 0;
+  for (const calibration of input) {
+    if (isCorrectCalibration(calibration, true))
+      resultpt2 += calibration.result;
+  }
+  console.log("Result Pt 2: ", resultpt2); // Part 1 answer
 }
 
 main();
